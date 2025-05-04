05af55291272db4f77b9a6c9d23e4003f82de23a
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 12:58:57 -0300

    feat(rotas): adiciona rotas para cadastro e listagem de clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CadastrarComponent } from './clientes/cadastrar/cadastrar.component';
+import { ListarComponent } from './clientes/listar/listar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'cadastro', component: CadastrarComponent },
+    { path: 'lista', component: ListarComponent }
+];
