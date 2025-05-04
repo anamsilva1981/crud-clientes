fccacc6d57f33ead457a32d3bc0730a2c3e8d60e
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 13:42:11 -0300

    feat(cadastrar): adiciona FlexLayoutModule e MatCardModule ao componente Cadastrar clientes

--- src/app/clientes/cadastrar/cadastrar.component.ts
+++ src/app/clientes/cadastrar/cadastrar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [],
+  imports: [FlexLayoutModule, MatCardModule],
   templateUrl: './cadastrar.component.html',
   styleUrl: './cadastrar.component.scss'
 })
