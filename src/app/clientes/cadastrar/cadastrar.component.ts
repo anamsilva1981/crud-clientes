4ad4740c1a9eb8969c433cd63dad17e1fb981bdd
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 21:57:10 -0300

    feat(cadastrar): adiciona MatIconModule e MatButtonModule ao componente cadastro de clientes

--- src/app/clientes/cadastrar/cadastrar.component.ts
+++ src/app/clientes/cadastrar/cadastrar.component.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cadastrar',
@@ -13,6 +15,8 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './cadastrar.component.html',
   styleUrl: './cadastrar.component.scss',
