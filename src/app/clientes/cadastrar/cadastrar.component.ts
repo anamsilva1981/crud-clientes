478aac1d5b81bbf23e35dd4a7a2f9c7877f78358
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 21:34:50 -0300

    feat(cadastrar): adiciona FormsModule, MatFormFieldModule e MatInputModule ao componente Cadastrar

--- src/app/clientes/cadastrar/cadastrar.component.ts
+++ src/app/clientes/cadastrar/cadastrar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [FlexLayoutModule, MatCardModule],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './cadastrar.component.html',
-  styleUrl: './cadastrar.component.scss'
+  styleUrl: './cadastrar.component.scss',
 })
-export class CadastrarComponent {
-
-}
+export class CadastrarComponent {}
