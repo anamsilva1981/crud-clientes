5007fa54d5f6d7527a3e79479f46ce6f78f2e85c
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-05 08:03:09 -0300

    feat(cliente): adiciona método obterStorage para gerenciar clientes no localStorage

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -6,7 +6,26 @@ import { Cliente } from './cliente';
 })
 export class ClienteService {
 
+  static REPO_CLIENTES = "_CLIENTES"
+
   public salvarCliente(cliente: Cliente): void{
     console.log(cliente, "salvarCliente");
   }
+
+  obterStorage(): Cliente[] {
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes)
+      return clientes;
+    }
+    
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
+  }
+
+
+
+
+
 }
