3723da43e76d987325cac98c88c1003b9875f6ab
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-05 09:47:29 -0300

    feat(listar): refatora componente Listar clientes para implementar OnInit e carregar lista de clientes

--- src/app/clientes/listar/listar.component.ts
+++ src/app/clientes/listar/listar.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-listar',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './listar.component.html',
-  styleUrl: './listar.component.scss'
+  styleUrl: './listar.component.scss',
 })
-export class ListarComponent {
+export class ListarComponent implements OnInit {
+
+  public listaClientes: Cliente[] = [];
+
+  private clienteService = inject(ClienteService);
+
+  public ngOnInit(): void {
+    this.listaClientes = this.clienteService.pesquisarCliente('');
+    console.log(this.listaClientes, 'Lista de clientes');
+  }
+
 
 }
