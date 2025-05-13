8dd10e082ad51fe33dacc7f7bc7069fb92307735
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-13 14:36:29 -0300

    feat(cadastrar): implementa lógica para carregar no formulario o cliente por ID ao iniciar o componente

--- src/app/clientes/cadastrar/cadastrar.component.ts
+++ src/app/clientes/cadastrar/cadastrar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, Query } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Cliente } from './../cliente';
 import { ClienteService } from '../cliente.service';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
@@ -19,20 +19,37 @@ import { ClienteService } from '../cliente.service';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   templateUrl: './cadastrar.component.html',
   styleUrl: './cadastrar.component.scss',
 })
-export class CadastrarComponent {
-
+export class CadastrarComponent implements OnInit {
   private clienteService = inject(ClienteService);
+  private route = inject(ActivatedRoute);
 
   public cliente: Cliente = Cliente.newCliente();
+  public atualizando: boolean = false;
 
-  
-  public salvarCliente(){
-    this.clienteService.salvarCliente(this.cliente);
+  public ngOnInit(): void {
+    this.carregarClientePorId();
   }
 
+  private carregarClientePorId(): void {
+    this.route.queryParamMap.subscribe((query: any) => {
+      const params = query['params'];
+      const id = params['id'];
+      if (id) {
+        this.atualizando = true;
+        this.cliente =
+          this.clienteService.buscarClientePorId(id) || Cliente.newCliente();
+      }
+    });
+  }
+
+  public salvarCliente() {
+    this.clienteService.salvarCliente(this.cliente);
+    this.cliente = Cliente.newCliente();
+    alert('Cliente salvo com sucesso!');
+  }
 }
